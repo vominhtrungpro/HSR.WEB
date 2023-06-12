9a5e3ab393daeb483a7fe7f7bc51af08b6023aec
vominhtrungpro <vominhtrungpro@gmail.com>
2023-06-12 16:10:17 +0700

    init axios

--- web/src/pages/Characters/Characters.js
+++ web/src/pages/Characters/Characters.js
@@ -1,12 +1,23 @@
-import React from 'react';
 import Navbar from '../../components/navbar';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 
 function Characters() {
+  const [filters, setFilter] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:1994/api/v1/elements/filter`)
+      .then((res) => {
+        setFilter(res.data);
+      });
+  }, []);
     return (
         <div className="App">
           <Navbar  />
           <div className="App-header">
-            Test
           </div>
         </div>
       );
